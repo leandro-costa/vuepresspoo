69447ce0cdc85003e1b55fe8130b0b9126b74d56
Leandro Souza <leandro.costa@ifba.edu.br>
2021-07-18 04:11:19 +0000

    novo deploy

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -27,27 +27,67 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "01_paradigmas/01_introducao.html",
-    "revision": "432cd34a9327f0c004baf41f22f293ec"
+    "revision": "256f7170c273a1aa9cb5f9a7e1416f7f"
   },
   {
     "url": "01_paradigmas/02_Linguagens.html",
-    "revision": "4e9f363e0f8fabf50eebda2d0f9e944e"
+    "revision": "3949dba5aea0f29ebc0455da6874e1ed"
   },
   {
     "url": "01_paradigmas/03_Paradigmas.html",
-    "revision": "7d5142073b3b1ab17e7692a7bec846e2"
+    "revision": "34511c83c2c2ccb55e2a7963e0013690"
   },
   {
     "url": "01_paradigmas/index.html",
-    "revision": "0b273bd99884c2633ce7ecd146b92e62"
+    "revision": "f7df33df20c8cd8956f7c54a0ea67645"
+  },
+  {
+    "url": "02_java/01_codificando.html",
+    "revision": "06a90c5b119db15ffc913e817da6e9f0"
+  },
+  {
+    "url": "02_java/02_if_else.html",
+    "revision": "d0e6d345f7b5b25cb5a610e3306b376a"
+  },
+  {
+    "url": "02_java/03_loops.html",
+    "revision": "2164c9e84a50b871bd2bc4c426d4e070"
+  },
+  {
+    "url": "02_java/04_escopo_bloco.html",
+    "revision": "72e1491958041463f4357f5e30d61766"
   },
   {
     "url": "02_java/index.html",
-    "revision": "f9b258de92de55f84ee7b2d24e8fd6ae"
+    "revision": "4252f58e179b92839bcb1435d9551681"
+  },
+  {
+    "url": "03_conceitos_OO/01_abstracao.html",
+    "revision": "522b5939d63845f4373845d0bea0545f"
+  },
+  {
+    "url": "03_conceitos_OO/02_objeto.html",
+    "revision": "f069cc13243cb149ce1a48bcd3af9689"
+  },
+  {
+    "url": "03_conceitos_OO/03_classe.html",
+    "revision": "09be1b293262dc9226b91d6ae8be951e"
+  },
+  {
+    "url": "03_conceitos_OO/04_referencia.html",
+    "revision": "38e8db130c1e62f80a47192b7c6f00c6"
+  },
+  {
+    "url": "03_conceitos_OO/05_outras_classes.html",
+    "revision": "59ab5053f1759d2c05425a164fada043"
+  },
+  {
+    "url": "03_conceitos_OO/index.html",
+    "revision": "4104204db37615d581a2c1da541baefe"
   },
   {
     "url": "404.html",
-    "revision": "5ba224f117876186dfac65abc5e22a63"
+    "revision": "f98210b7ee7da5b619a82dfd16de2a85"
   },
   {
     "url": "assets/css/0.styles.0105b20d.css",
@@ -62,44 +102,84 @@ self.__precacheManifest = [
     "revision": "cb2905725bfcc3de9514ccfa99022349"
   },
   {
-    "url": "assets/js/11.315f08c4.js",
-    "revision": "684a7a49253173c602279ca2de5c3d30"
+    "url": "assets/js/11.4b81de4d.js",
+    "revision": "d69eedc4edbf348e663202ff094cc696"
   },
   {
-    "url": "assets/js/12.788fd601.js",
-    "revision": "e9a5d96a85d2f3a93d336301b9152b84"
+    "url": "assets/js/12.9f070988.js",
+    "revision": "200b8ba5bcde155e961df4fdb8b8f513"
   },
   {
-    "url": "assets/js/13.967557b5.js",
-    "revision": "3a8529a1da586c13bb2043f0088de976"
+    "url": "assets/js/13.c789313c.js",
+    "revision": "71469533648ba1327eddebccd8e9d761"
   },
   {
-    "url": "assets/js/14.a3a51423.js",
-    "revision": "d4c3b23f64937c34e57758511da8e478"
+    "url": "assets/js/14.3581f008.js",
+    "revision": "18f099e4feee0cfe9e80c00a8c568cda"
   },
   {
-    "url": "assets/js/15.8b1cb5ae.js",
-    "revision": "7ec9f6f2a0cd4ca54c48a09afa255125"
+    "url": "assets/js/15.a23e3edb.js",
+    "revision": "c08fe76fc955157c51322e01795f03eb"
   },
   {
-    "url": "assets/js/16.8dd19acf.js",
-    "revision": "c4bfa03ee9ecc4e313b3e42a7c3d7990"
+    "url": "assets/js/16.a1611322.js",
+    "revision": "6413e2c7e6304dd3446802cbe4b58899"
   },
   {
-    "url": "assets/js/17.c1dc7228.js",
-    "revision": "d696fad63ba01bb1f7af52d3c3585b04"
+    "url": "assets/js/17.3db1092d.js",
+    "revision": "8ebf26f1ad5dde9749bb2d11a4cdeab9"
   },
   {
-    "url": "assets/js/2.42578c1c.js",
-    "revision": "ffd1b4a378c20b0a5711f29dde728c66"
+    "url": "assets/js/18.4099a1c7.js",
+    "revision": "05abe614bb1f81844092c6491012fae0"
   },
   {
-    "url": "assets/js/3.e0590093.js",
-    "revision": "a1494ad7cd6a2121b6d073c7b3c4611d"
+    "url": "assets/js/19.f05da336.js",
+    "revision": "ee8d7ca80f0ee48b43abecacbb4fa4d0"
   },
   {
-    "url": "assets/js/4.74c5d91a.js",
-    "revision": "03254874bce94e47094b4df2177ebb9d"
+    "url": "assets/js/2.1f6bfe3c.js",
+    "revision": "43f93ed3df64bc6273b41316f2a935a5"
+  },
+  {
+    "url": "assets/js/20.18140907.js",
+    "revision": "9e59c30e4f664e54312cde9d2d42f438"
+  },
+  {
+    "url": "assets/js/21.f5d49046.js",
+    "revision": "db1c850ccb46342c1f0e7411f67af51b"
+  },
+  {
+    "url": "assets/js/22.d1fa27a0.js",
+    "revision": "b1393141a2ceb7bf7403244011f26645"
+  },
+  {
+    "url": "assets/js/23.b22433ab.js",
+    "revision": "7ae682c751205c48d88d86881e0b6bee"
+  },
+  {
+    "url": "assets/js/24.304f9ce6.js",
+    "revision": "08dfcefa0476331c7c76effb6f1ba1dc"
+  },
+  {
+    "url": "assets/js/25.4dfc3e65.js",
+    "revision": "9dcc1bcce586ccd4364670fea34fb6b7"
+  },
+  {
+    "url": "assets/js/26.e0ca5116.js",
+    "revision": "d2c956d2a0fa339049b2b2df8b64ccfe"
+  },
+  {
+    "url": "assets/js/27.fb05b84a.js",
+    "revision": "94ccb68b838061f0fb6859a809fb65dd"
+  },
+  {
+    "url": "assets/js/3.214ec939.js",
+    "revision": "b7c185e811432ed5e7d507811d4613fb"
+  },
+  {
+    "url": "assets/js/4.7806bc0f.js",
+    "revision": "e28e8a96cd41398f4a383826b6111356"
   },
   {
     "url": "assets/js/5.da33bc20.js",
@@ -118,12 +198,12 @@ self.__precacheManifest = [
     "revision": "e07c16156cbd284fd5bf8f32dd45d8dd"
   },
   {
-    "url": "assets/js/9.17da1784.js",
-    "revision": "d6dc812de2614e0d9f65ea7a849f68c7"
+    "url": "assets/js/9.5d5874ab.js",
+    "revision": "fe9339af19db157afb3f73d72924a9ff"
   },
   {
-    "url": "assets/js/app.2051be17.js",
-    "revision": "a3624675adee0c1f46e69cf5b2892d46"
+    "url": "assets/js/app.6144de85.js",
+    "revision": "e19b2c936859dbf7b44d231237c1484b"
   },
   {
     "url": "icon-192x192.png",
@@ -143,7 +223,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "ed57723614b44b8e7b904b14367ac324"
+    "revision": "2d8f2f214c7032f6d700253a92781513"
   },
   {
     "url": "logo.png",
